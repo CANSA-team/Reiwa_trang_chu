7c9eae7266bf5d7d694963263591016f043619e3
NatswarChuan <natswarchuan@gmail.com>
2021-05-02 16:26:55 +0700

    update

--- models/NavBarModel.php
+++ models/NavBarModel.php
@@ -1,10 +1,18 @@
 <?php
 class NavBarModel extends Db
 {
-    //lấy danh sách banner
+    //lấy danh sách NavBar
     public function getNavBar()
     {
         $sql = parent::$conection->prepare('SELECT * FROM `navbar`');
         return parent::select($sql);
+    }
+
+    //lấy danh sách DropdownNavBar
+    public function getDropdownNavBar($id)
+    {
+        $sql = parent::$conection->prepare('SELECT * FROM `dropdown` WHERE `id`=?');
+        $sql->bind_param('i',$id);
+        return parent::select($sql)[0]['url'];
     }
 }
--- view/template/navbar.php
+++ view/template/navbar.php
@@ -1,5 +1,11 @@
 <?php
 $navbar = setNavBar();
 foreach ($navbar as $item) {
-    echo $item['url'];
+    $dropdown = '';
+    if(!empty($item['dropdown_id'])){
+        $navbarModel = new NavBarModel();
+        $dropdown = $navbarModel->getDropdownNavBar($item['dropdown_id']);
+    }
+    $content = '<li class="nav-item "><a href="'.$item['url'].'" class="nav-text-style nav-padding is-active">'.$item['name'].'</a>'.$dropdown.'</li>';
+    echo $content;
 }
